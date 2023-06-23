a96aeddf95f904a6e22738e6e325fbee53e8a0f4
Documenter.jl <documenter@juliadocs.github.io>
2023-06-23 11:05:26 +0000

    build based on 8b50e2d

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = SubmitPaper","category":"page"},{"location":"#SubmitPaper","page":"Home","title":"SubmitPaper","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for SubmitPaper.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [SubmitPaper]","category":"page"}]
+}
